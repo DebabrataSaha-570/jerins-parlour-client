e39e9663ec6b1d886acd36d75d60800d59815369
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2022-01-01 00:56:50 +0600

    login and registration page completed

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,11 +1,26 @@
+import { Box, Button, Typography, Container } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navigation from '../../Shared/Navigation/Navigation';
 
 const Login = () => {
     return (
         <>
             <Navigation></Navigation>
-            <h3>This is login component</h3>
+            <Container>
+                <Box style={{ marginTop: '250px' }} sx={{ width: '35%', mx: 'auto', mb: 5, }}>
+                    <Typography sx={{ textAlign: 'center', my: 3 }} variant="h6">Login With</Typography>
+                    <Button sx={{ display: 'flex', justifyContent: 'flex-start', width: '100%', border: 1, borderColor: 'grey.500', borderRadius: '30px' }} style={{ color: 'black', textTransform: 'capitalize' }}>
+                        <img style={{ marginRight: '100px' }} width="35px" src="https://img.icons8.com/color/48/000000/google-logo.png" />
+
+                        <Typography variant="subtitle1">Continue With Google</Typography>
+
+                    </Button>
+                    <Typography sx={{ mt: 2 }} style={{ textAlign: 'center' }} variant="subtitle1" gutterBottom component="div">
+                        Don't Have an account?  <Link to="/register">Create an account</Link>
+                    </Typography>
+                </Box>
+            </Container>
         </>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,6 +1,7 @@
-import { Button, Container, TextField, Typography } from '@mui/material';
+import { Button, Container, MenuItem, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navigation from '../../Shared/Navigation/Navigation';
 
 const Register = () => {
@@ -8,7 +9,7 @@ const Register = () => {
         <>
             <Navigation></Navigation>
             <Container>
-                <Box sx={{ width: '35%', mx: 'auto', mt: 5, border: '1.5px solid lightGray', p: 5 }}>
+                <Box sx={{ width: '35%', mx: 'auto', mt: 5, border: '1.5px solid lightGray', py: 4, px: 5 }}>
                     <Typography
                         variant="h6"
                     >
@@ -26,8 +27,26 @@ const Register = () => {
 
                         <Button sx={{ width: '100%' }} variant="contained" type="submit">Create an Account</Button>
                     </form>
-
+                    <Typography sx={{ mt: 2 }} style={{ textAlign: 'center' }} variant="subtitle1" gutterBottom component="div">
+                        Already Have an account?  <Link to="/login">Login</Link>
+                    </Typography>
                 </Box>
+
+                <Box sx={{ width: '35%', mx: 'auto', my: 2 }} style={{ display: 'flex', alignItems: 'center' }}>
+                    <span style={{ flexGrow: 1, borderBottom: '1px solid lightGray', margin: '5px' }}></span>
+                    <span>Or</span>
+                    <span style={{ flexGrow: 1, borderBottom: '1px solid lightGray', margin: '5px' }}></span>
+                </Box>
+
+                <Box sx={{ width: '35%', mx: 'auto', mb: 5 }}>
+                    <Button sx={{ display: 'flex', justifyContent: 'flex-start', width: '100%', border: 1, borderColor: 'grey.500', borderRadius: '30px' }} style={{ color: 'black', textTransform: 'capitalize' }}>
+                        <img style={{ marginRight: '110px' }} width="35px" src="https://img.icons8.com/color/48/000000/google-logo.png" />
+
+                        <Typography variant="subtitle1">Continue With Google</Typography>
+
+                    </Button>
+                </Box>
+
             </Container>
         </>
     );
